c6cde9d33c6c4726b6566954c5d06d49bd82952c
Tony <waiting33118@gmail.com>
2020-05-04 22:30:14 +0800

    build standard env

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const exphdbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const hostname = '127.0.0.1'
+const port = 3000
+
+app.engine('handlebars', exphdbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+	res.render('home')
+})
+
+app.listen(port, hostname, () => {
+	console.log(`The server is running on http://${hostname}:${port}`)
+})
